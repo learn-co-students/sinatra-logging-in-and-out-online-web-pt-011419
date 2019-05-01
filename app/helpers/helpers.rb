0039d52eff3e004a0f0f8994207a38b383acf658
dylanclements <dclements@protonmail.com>
2019-05-01 12:39:46 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
-class Helpers
+require 'pry'
+
+class Helpers < ActiveRecord::Base
+
+    def self.current_user(session)
+       user = User.find(session[:user_id])
+       #binding.pry
+    end
+
+    def self.is_logged_in?(session)
+        session[:user_id] != nil
+    end
 end
